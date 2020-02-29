59a40db61926d529d1619d5aee0a014468816ddf
Earth <earth@Earths-MacBook-Pro.local>
2020-02-29 16:23:05 +0700

    dont change any thing

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,7 +10,7 @@ print(InputYoutCcore!)
 2+10
 3*18
 4 % 5
-34.0 .truncatingRemainder(dividingBy: 12.2)
+34.0 .truncatingRemainder(dividingBy: 12.2) // ใช้หา mod สำหับที่เป็นทศนิยม
 sin(7 * Double.pi )
 7 * Double.pi
 
@@ -18,4 +18,34 @@ let someValue:Int = 10
 var tenMilion = 1_000_000
 print(tenMilion)
 
+
+let testNumber:Int = 11
+if ( testNumber % 2 == 0)
+{
+    print("even")
+}
+else {
+    print("zero")
+}
+
+var indiger:Int = 10
+let decimal:Double = 20.1255
+
+var conevnt:Int = Int(decimal)
+
+var avg1 = 10
+var avg2 = 13
+var ans2 = Double(avg1)/2
+
+
+let NameDog:String = "dog"
+
+var coordinate = (x:21.23 , y:23.34)
+let someNumber = coordinate.x
+
+let a = 5
+let b = 10
+let min = a < b ? a:b
+let max = a > b ? a:b
+print(max)
 
