7525b4ae21f4a7745079da6292c7149492bb107e
Earth <earth@Earths-MacBook-Pro.local>
2020-02-29 14:50:53 +0700

    test let var

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,21 @@
 import UIKit
 
 var str = "Hello, playground"
-print("sdfsfd");
+print("into Swift code");
 print(str);
 var InputYoutCcore:Int? = 30
 print(InputYoutCcore!)
+
+
 2+10
 3*18
 4 % 5
 34.0 .truncatingRemainder(dividingBy: 12.2)
 sin(7 * Double.pi )
 7 * Double.pi
+
+let someValue:Int = 10
+var tenMilion = 1_000_000
+print(tenMilion)
+
 
